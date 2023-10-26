4b9539d30c165f3ab8050de7aeb92eb4d46e45f5
unknown <tonyunday@gmail.com>
2023-10-26 09:59:07 -0700

    Add calculator setup

---
+++ app.js
@@ -0,0 +1,4 @@
+document.getElementById('card').innerHTML= 'My Calculator';
+document.getElementById('board').style.backgroundColor = 'blue';
+document.getElementsByClassName('board').style.height = '10px';
+document.getElementsByClassName('board').style.width = '10px';
